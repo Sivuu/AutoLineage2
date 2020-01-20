9d03b15749a716095450e196e6760e2b0fc07647
Mr.Trinh <biofan4u@hotmail.com>
2020-01-21 05:31:23 +0900

    GUIv2: Finish GUI, algorthim continous

--- AutoL2Bot.py
+++ AutoL2Bot.py
@@ -1,97 +1,12 @@
 import os
 import sys
 import time
-import ctypes
 import wx
-import threading
-import keyboard
-import win32gui
-from Color import Color
-from Keyboard import Keyboard
+import ctypes
 from Gui import UserGui
 
-
-def ColorCheckThread():
-    try:
-        while objGui:
-            if objGui.tab["RunCheck"] :
-                for i in range(1,10):
-                    if objGui.tab["Tab %s" % i]["ColorCheckBox"].GetValue() and objGui.tab["Tab %s" % i]["Color"]:
-                        for color in objGui.tab["Tab %s" % i]["Color"]:
-                            if (not objColor.CheckPixelColor(color)) and objGui.tab["Handler"]:
-                                for hwnd in objGui.tab["Handler"]:
-                                    objKeyBoard.ControlSend(objGui.tab["Tab %s" % i], hwnd, objGui.tab["Tab %s" % i]["Key"].GetValue())
-                                time.sleep(1)
-            else:
-                pass
-    except NameError as er:
-        print("Error in color check thread is: %s" % (er))
-
-def RepeatKeyThread():
-    try:
-        while objGui:
-            if objGui.tab["RunCheck"]:
-                for i in range(1,10):
-                    if objGui.tab["Tab %s" % i]["RepeatKeyBox"].GetValue():
-                        try:
-                            cdtime = int(float(objGui.tab["Tab %s" % i]["Time"].GetValue()))
-                        except:
-                            pass
-                        if cdtime > 0:
-                            lhtime = objGui.tab["Tab %s" % i]["LastHitTime"]
-                            if (lhtime + cdtime) < time.time():
-                                lhtime = objGui.tab["Tab %s" % i]["LastHitTime"] = time.time()
-                                try:
-                                    sltime = int(float(objGui.tab["Tab %s" % i]["Sleep"].GetValue()))
-                                except:
-                                    pass
-                                for hwnd in objGui.tab["Handler"]:
-                                    objKeyBoard.ControlSend(objGui.tab["Tab %s" % i], hwnd, objGui.tab["Tab %s" % i]["Key"].GetValue())
-                                time.sleep(sltime)
-            else:
-                pass
-
-    except NameError as er:
-        print("Error in Repeat key thread is: %s" % (er))
-
-def HotKeyRegistry():
-    for i in range(1,10):
-        keyboard.add_hotkey('ctrl+%s' % i, AddColor, args=[i])
-        keyboard.add_hotkey('f%s' % i, ControlSend, args=[i], suppress=True)
-    keyboard.wait("ctrl+shift+end")
-
-def AddColor(value):
-    if objGui:
-        objGui.tab["Tab %s" % value]["Color"].append(   
-            objColor.GetPixelColor()
-        )
-    else:
-        pass
-
-def ControlSend(value):
-    if objGui:
-        if objGui.tab["HotKey"]:
-            for hwnd in objGui.tab["Handler"]:
-                objKeyBoard.ControlSend(objGui.tab["Tab %s" % value], hwnd, objGui.tab["Tab %s" % value]["Key"].GetValue())
-        else:
-            pass
-
 if __name__ == '__main__' and ctypes.windll.shell32.IsUserAnAdmin():
-    # Check HDD -----
-    myuuid = os.popen("wmic diskdrive get serialnumber").read().split()[-1]
-    #myuuid = '2H4520037435'
-    # Thuc hien threading o day
-    global objColor, objGui, objKeyBoard, app
-    objColor = Color()
-    objKeyBoard = Keyboard()
-
     app = wx.App(False)
-    objGui = UserGui(None, "Loop Macro Lineage 2 - %s" % myuuid, myuuid)
+    objGui = UserGui(None, "Loop Macro Lineage 2 - %s" % os.popen("wmic diskdrive get serialnumber").read().split()[-1])
     # Khai bao cac threading tai day
-    thotkey = threading.Thread(target= HotKeyRegistry, daemon=True)
-    thotkey.start()
-    tcolor = threading.Thread(target= ColorCheckThread, daemon=True)
-    tcolor.start()
-    trepeat = threading.Thread(target= RepeatKeyThread, daemon=True)
-    trepeat.start() 
     app.MainLoop()
--- Gui/__init__.py
+++ Gui/__init__.py
@@ -1,104 +1,108 @@
-import time
 import os
+import sys
+import time
 import win32api
 import win32gui
 import win32con
 import wx
-import sys
+import threading
 import base64
 import ntplib
 from datetime import datetime, timezone, timedelta
-#V0wxNVYyQzg=
 
 class UserGui(wx.Frame):
-    def __init__(self, parent, title, uuid):
-        # Khai bao phuong thuc bao mat
-        self.encode = uuid
+    def __init__(self, parent, title):
+        # Khai bao tham so cua GUI
+        self.encode = os.popen("wmic diskdrive get serialnumber").read().split()[-1]
         f = open(os.path.join(os.path.abspath(os.getcwd()),"key.txt"),"r")
         keytemp =  base64.b64decode(f.read().encode("utf-8")).decode("utf-8")
         self.keyCode = keytemp.split(";")[0]
         self.keyTime = keytemp.split(";")[1] + "+00:00"
+        self.UIDict = dict()
 
         # Khoi tao cua so ung dung
-        wx.Frame.__init__(self,parent, title=title, size= wx.Size(460,350))
-        mainpn = wx.Panel(self)
-        mainnb = wx.Notebook(mainpn)
-        self.tab = dict()
-        self.tab["RunCheck"] = False
-        self.tab["HotKey"] = False
-        self.tab["Handler"] = self.GetHWND()
-
-        # Tab Control ung dung
-        self.tab["MainPage"] = wx.Panel(mainpn)
-        sizerControl = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.btRun = wx.Button(self.tab["MainPage"], wx.ID_ANY, u"Start")
-        self.Bind(wx.EVT_BUTTON, self.RunClick, self.btRun)
-
-        self.btHotKey = wx.Button(self.tab["MainPage"], wx.ID_ANY, u"HotKey")
-        self.Bind(wx.EVT_BUTTON, self.HotKeyClick, self.btHotKey)
-
-        self.btExit = wx.Button(self.tab["MainPage"], wx.ID_ANY, u"Exit")
-        self.Bind(wx.EVT_BUTTON, self.ExitClick, self.btExit)
-    
-        sizerControl.AddStretchSpacer()
-        sizerControl.Add(self.btRun, 0, wx.ALIGN_CENTER_HORIZONTAL, 0)
-        sizerControl.Add(self.btHotKey, 0,wx.ALIGN_CENTRE_HORIZONTAL, 0)
-        sizerControl.Add(self.btExit, 0,wx.ALIGN_CENTRE_HORIZONTAL, 0)
-        sizerControl.AddStretchSpacer()
-        self.tab["MainPage"].SetSizer(sizerControl)
-
-        # Tab danh cho moi key
-        for i in range(9):
-            self.tab["Tab %s" % (i+1)] = dict()
-            self.tab["Tab %s" % (i+1)]["Panel"] = wx.Panel(mainnb)
-            # Key to send
-            self.tab["Tab %s" % (i+1)]["LabelKey"] = wx.StaticText(self.tab["Tab %s" % (i+1)]["Panel"],  wx.ID_ANY, "Key Name %s" % (i+1), pos=(5, 10), size=(80, 30))
-            self.tab["Tab %s" % (i+1)]["Key"] = wx.TextCtrl(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, "f%s" % (i+1), pos=( 95, 5), size=(80, 30))
-
-            # Repeat time
-            self.tab["Tab %s" % (i+1)]["LabelTime"] = wx.StaticText(self.tab["Tab %s" % (i+1)]["Panel"],  wx.ID_ANY, "Repeat Time %s" % (i+1), pos=(5, 50), size=(80, 30))
-            self.tab["Tab %s" % (i+1)]["Time"] = wx.TextCtrl(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, "600", pos=( 95, 45), size=(80, 30))
-            self.tab["Tab %s" % (i+1)]["LastHitTime"] = time.time()
-            # Sleep time
-            self.tab["Tab %s" % (i+1)]["LabelSleep"] = wx.StaticText(self.tab["Tab %s" % (i+1)]["Panel"],  wx.ID_ANY, "Sleep Time %s" % (i+1), pos=(5, 90), size=(80, 30))
-            self.tab["Tab %s" % (i+1)]["Sleep"] = wx.TextCtrl(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, "2", pos=( 95, 85), size=(80, 30))
-
-            # Check box to active
-            self.tab["Tab %s" % (i+1)]["ColorCheckBox"] = wx.CheckBox(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, "Color Check", pos=(5, 125))
-            self.tab["Tab %s" % (i+1)]["RepeatKeyBox"] = wx.CheckBox(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, "Repeat Key", pos=(165, 125))
-            
-            # Clear color
-            self.tab["Tab %s" % (i+1)]["ButtonClear"] = wx.Button(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, u"Clear Color", pos=(5, 165))
-            self.Bind(wx.EVT_BUTTON, lambda event,temp=i+1 : self.ClearColor(event,temp), self.tab["Tab %s" % (i+1)]["ButtonClear"])
-
-            # Get client to send for key
-            self.tab["Tab %s" % (i+1)]["ButtonHandle"] = wx.Button(self.tab["Tab %s" % (i+1)]["Panel"], wx.ID_ANY, u"Set Windows", pos=(105, 165))
-            self.Bind(wx.EVT_BUTTON, lambda event,temp=i+1 : self.GetHandle(event,temp), self.tab["Tab %s" % (i+1)]["ButtonHandle"])
-
-            self.tab["Tab %s" % (i+1)]["Color"] = list()
-
-            for hwnd in self.tab["Handler"]:
-                self.tab["Tab %s" % (i+1)][hwnd] = True
-
-            tabSizer = wx.BoxSizer(wx.VERTICAL)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["LabelKey"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["Key"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["LabelTime"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["Time"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["LabelSleep"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["Sleep"], 0, wx.ALL, 5)
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["ColorCheckBox"], 0, wx.ALL, 5 )
-            tabSizer.Add(self.tab["Tab %s" % (i+1)]["ButtonClear"], 0, wx.ALL, 5 )
-
-            mainnb.AddPage(self.tab["Tab %s" % (i+1)]["Panel"], 'Group {}(f{})'.format(i+1,i+1))
-            
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(mainnb, 1, wx.EXPAND)
-        sizer.Add(self.tab["MainPage"])
-        mainpn.SetSizer(sizer)
-
+        wx.Frame.__init__(self,parent, title=title, size= wx.Size(750,420), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
         self.CreateStatusBar()
+        sizerFrame = wx.GridBagSizer(0,0)
+        sizerFrame.SetFlexibleDirection(wx.BOTH)
+        sizerFrame.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+
+        # Choice box de lua chon Handle setup cho tool
+        self.UIDict["Handle"] = []
+        self.UIDict["ChoiceHandle"] = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, self.UIDict["Handle"], 0 )
+        self.UIDict["ChoiceHandle"].SetSelection( 0 )
+        sizerFrame.Add(self.UIDict["ChoiceHandle"], wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+
+        # Button Start/Run
+        self.UIDict["btRun"] = wx.Button(self, wx.ID_ANY, label='Start')
+        sizerFrame.Add(self.UIDict["btRun"] , wx.GBPosition( 2, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        # Button Get Handle
+        self.UIDict["btGetHandle"] = wx.Button(self, wx.ID_ANY, label='Get Handle')
+        sizerFrame.Add(self.UIDict["btGetHandle"] , wx.GBPosition( 3, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        # Button Save Key
+        self.UIDict["btSaveKey"] = wx.Button(self, wx.ID_ANY, label='Save Key')
+        sizerFrame.Add(self.UIDict["btSaveKey"] , wx.GBPosition( 4, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        # Button Save Profile
+        self.UIDict["btSaveProfile"] = wx.Button(self, wx.ID_ANY, label='Save Profile')
+        sizerFrame.Add(self.UIDict["btSaveProfile"] , wx.GBPosition( 6, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        # Button Load Profile
+        self.UIDict["btLoadProfile"] = wx.Button(self, wx.ID_ANY, label='Load Profile')
+        sizerFrame.Add(self.UIDict["btLoadProfile"] , wx.GBPosition( 7, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+        # Button Exit
+        self.UIDict["btExit"] = wx.Button(self, wx.ID_ANY, label='Exit')
+        sizerFrame.Add(self.UIDict["btExit"] , wx.GBPosition( 9, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
+
+        # Thiet lap Panel danh cho Handle Infomation --------------------
+        self.UIDict["panelHandle"] = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        sizerFrame.Add(self.UIDict["panelHandle"], wx.GBPosition( 0, 1 ), wx.GBSpan( 19, 47 ), wx.ALL, 5 )
+        
+        sizerPanel = wx.GridBagSizer(0,0)
+        sizerPanel.SetFlexibleDirection(wx.BOTH)
+        sizerPanel.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+        # Label Name cua Lineage II Tab
+        self.UIDict["panelHandleName"] = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Lineage 2 Tab")
+
+        # Nhom Label KeyCombine Information
+        pnKeyName = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Key Combine")
+        sizerPanel.Add(pnKeyName,wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL, 5 )
+        pnRepeat = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Repeat Time")
+        sizerPanel.Add(pnRepeat,wx.GBPosition(1, 1), wx.GBSpan(1, 1), wx.ALL, 5 )
+        pnSleep = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Sleep Time")
+        sizerPanel.Add(pnSleep,wx.GBPosition(1, 2), wx.GBSpan(1, 1), wx.ALL, 5 )
+        pnActiveRepeat = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Active Repeat")
+        sizerPanel.Add(pnActiveRepeat,wx.GBPosition(1, 3), wx.GBSpan(1, 1), wx.ALL, 5 )
+        pnActiveColor = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Active Color")
+        sizerPanel.Add(pnActiveColor,wx.GBPosition(1, 4), wx.GBSpan(1, 1), wx.ALL, 5 )
+        pnActiveHotKey = wx.StaticText(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Active HotKey")
+        sizerPanel.Add(pnActiveHotKey,wx.GBPosition(1, 5), wx.GBSpan(1, 1), wx.ALL, 5 )
+
+        self.UIDict["CombineKey"] = dict()
+        for i in range(2,11):
+            self.UIDict["CombineKey"]["Key%s" %(i-1)] = dict()
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnKeyName"] = wx.TextCtrl(self.UIDict["panelHandle"], id=wx.ID_ANY, value="f%s" %(i-1))
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnKeyName"] ,wx.GBPosition(i, 0), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnRepeat"] = wx.TextCtrl(self.UIDict["panelHandle"], id=wx.ID_ANY, value="5")
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnRepeat"],wx.GBPosition(i, 1), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnSleep"] = wx.TextCtrl(self.UIDict["panelHandle"], id=wx.ID_ANY, value="5")
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnSleep"],wx.GBPosition(i, 2), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveRepeat"] = wx.CheckBox(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Y/N")
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveRepeat"],wx.GBPosition(i, 3), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveColor"] = wx.CheckBox(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Y/N")
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveColor"],wx.GBPosition(i, 4), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+
+            self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveHotKey"] = wx.CheckBox(self.UIDict["panelHandle"], id=wx.ID_ANY, label="Y/N")
+            sizerPanel.Add(self.UIDict["CombineKey"]["Key%s" %(i-1)]["pnActiveHotKey"],wx.GBPosition(i, 5), wx.GBSpan(1, 1), wx.ALL | wx.CENTER, 5 )
+        
+        self.UIDict["panelHandle"].SetSizer(sizerPanel)
+        self.UIDict["panelHandle"].Layout()
+        # Thiet lap Sizer Main-------------------------------------------
+        self.SetSizer(sizerFrame)
+        self.Layout()
         self.Show(True)
 
     def TakeNTPTime(self):
@@ -119,6 +123,18 @@ class UserGui(wx.Frame):
         else:
             return dateret 
 
+    def CheckKeyCode(self):
+        """ Kiem tra KeyCode co hop le"""
+        self.encode = os.popen("wmic diskdrive get serialnumber").read().split()[-1]
+        f = open(os.path.join(os.path.abspath(os.getcwd()),"key.txt"),"r")
+        keytemp =  base64.b64decode(f.read().encode("utf-8")).decode("utf-8")
+        self.keyCode = keytemp.split(";")[0]
+        self.keyTime = keytemp.split(";")[1] + "+00:00"
+        if (self.encode == self.keyCode) and (self.TakeNTPTime() < self.ConvertStringtoDateTime(self.keyTime)):
+            return True
+        else:
+            return False
+
     def CallBack(self, hwnd, hwnds):
         """Return a list of window handlers based on it class name"""
         if win32gui.GetClassName(hwnd) == "L2UnrealWWindowsViewportWindow"\
@@ -134,7 +150,7 @@ class UserGui(wx.Frame):
 
     def RunClick(self, event):
         """Event for Run Check when its clicked"""
-        if (self.encode == self.keyCode) and (self.TakeNTPTime() < self.ConvertStringtoDateTime(self.keyTime)):
+        if self.CheckKeyCode():
         # if True:
             if not self.tab["RunCheck"]: 
                 self.tab["RunCheck"] = True
@@ -147,7 +163,7 @@ class UserGui(wx.Frame):
 
     def HotKeyClick(self, event):
         """Event for Hot Key when its clicked"""
-        if (self.encode == self.keyCode) and (self.TakeNTPTime() < self.ConvertStringtoDateTime(self.keyTime)):
+        if self.CheckKeyCode():
         # if True:
             if not self.tab["HotKey"]:
                 self.tab["HotKey"] = True
@@ -157,6 +173,9 @@ class UserGui(wx.Frame):
                 self.btHotKey.SetLabel("Run Hot Key")
         else:
             wx.MessageBox("Incorrect key or Time is expired", "New Key Code Require", wx.OK)
+
+    def ColorClick(self):
+        pass
 
     def ExitClick(self, event):
         """Event for Exit Button when its clicked"""
@@ -188,3 +207,69 @@ class UserGui(wx.Frame):
         sizer.Add(okBtn)
         askFrame.SetSizer(sizer)
         askFrame.Show(True)
+
+    def ColorCheckThread(self):
+        try:
+            while objGui:
+                if objGui.tab["RunCheck"] :
+                    for i in range(1,10):
+                        if objGui.tab["Tab %s" % i]["ColorCheckBox"].GetValue() and objGui.tab["Tab %s" % i]["Color"]:
+                            for color in objGui.tab["Tab %s" % i]["Color"]:
+                                if (not objColor.CheckPixelColor(color)) and objGui.tab["Handler"]:
+                                    for hwnd in objGui.tab["Handler"]:
+                                        objKeyBoard.ControlSend(objGui.tab["Tab %s" % i], hwnd, objGui.tab["Tab %s" % i]["Key"].GetValue())
+                                    time.sleep(1)
+                else:
+                    pass
+        except NameError as er:
+            print("Error in color check thread is: %s" % (er))
+
+    def RepeatKeyThread(self):
+        try:
+            while objGui:
+                if objGui.tab["RunCheck"]:
+                    for i in range(1,10):
+                        if objGui.tab["Tab %s" % i]["RepeatKeyBox"].GetValue():
+                            try:
+                                cdtime = int(float(objGui.tab["Tab %s" % i]["Time"].GetValue()))
+                            except:
+                                pass
+                            if cdtime > 0:
+                                lhtime = objGui.tab["Tab %s" % i]["LastHitTime"]
+                                if (lhtime + cdtime) < time.time():
+                                    lhtime = objGui.tab["Tab %s" % i]["LastHitTime"] = time.time()
+                                    try:
+                                        sltime = int(float(objGui.tab["Tab %s" % i]["Sleep"].GetValue()))
+                                    except:
+                                        pass
+                                    for hwnd in objGui.tab["Handler"]:
+                                        objKeyBoard.ControlSend(objGui.tab["Tab %s" % i], hwnd, objGui.tab["Tab %s" % i]["Key"].GetValue())
+                                    time.sleep(sltime)
+                else:
+                    pass
+
+        except NameError as er:
+            print("Error in Repeat key thread is: %s" % (er))
+
+    def HotKeyRegistry(self):
+        for i in range(1,10):
+            keyboard.add_hotkey('ctrl+%s' % i, AddColor, args=[i])
+            keyboard.add_hotkey('f%s' % i, ControlSend, args=[i], suppress=True)
+        keyboard.wait("ctrl+shift+end")
+
+    def AddColor(self, value):
+        if objGui:
+            objGui.tab["Tab %s" % value]["Color"].append(   
+                objColor.GetPixelColor()
+            )
+        else:
+            pass
+
+    def ControlSend(self, value):
+        if objGui:
+            if objGui.tab["HotKey"]:
+                for hwnd in objGui.tab["Handler"]:
+                    objKeyBoard.ControlSend(objGui.tab["Tab %s" % value], hwnd, objGui.tab["Tab %s" % value]["Key"].GetValue())
+            else:
+                pass
+
