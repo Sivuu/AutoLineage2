447e29deaaef7240eb3119f417898da089c5acc3
Mr.Trinh <biofan4u@hotmail.com>
2020-02-17 12:12:04 +0900

    Good version for read HP party MP party

--- ReadMemory.py
+++ ReadMemory.py
@@ -34,13 +34,13 @@ GetLastError = windll.kernel32.GetLastError
 ## ReadProcessMemory
 ReadProcessMemory = windll.kernel32.ReadProcessMemory
 
-def GetBaseAddressModule(ProcessID, ModuleName):
+def GetBaseAddressModule(ProcessID, ModuleName, FirstOffSet):
     hModuleSnap = c_void_p(0)
-    me32 = MODULEENTRY32()
-    me32.dwSize = sizeof(MODULEENTRY32)
+    moduleEntry = MODULEENTRY32()
+    moduleEntry.dwSize = sizeof(MODULEENTRY32)
     hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, ProcessID )
 
-    ret = Module32First(hModuleSnap, byref(me32))
+    ret = Module32First(hModuleSnap, byref(moduleEntry))
     if ret == 0 :
         print ('ListProcessModules() Error on Module32First[%d]' % GetLastError())
         CloseHandle( hModuleSnap )
@@ -48,48 +48,57 @@ def GetBaseAddressModule(ProcessID, ModuleName):
 
     while ret :
         compare = ModuleName.encode()
-        if me32.szModule == compare:
-            print("   MODULE NAME:     %s"%             me32.szModule )
-            print("   executable     = %s"%             me32.szExePath )
-            print("   process ID     = 0x%08X"%         me32.th32ProcessID )
-            print("   ref count (g)  =     0x%04X"%     me32.GlblcntUsage )
-            print("   ref count (p)  =     0x%04X"%     me32.ProccntUsage )
-            print("   base address   = {}".format(me32.modBaseAddr))
-            print("   base size      = %d"%             me32.modBaseSize )
+        if moduleEntry.szModule == compare:
+            print("   MODULE NAME:     %s"%             moduleEntry.szModule )
+            print("   executable     = %s"%             moduleEntry.szExePath )
+            print("   process ID     = 0x%08X"%         moduleEntry.th32ProcessID )
+            print("   ref count (g)  =     0x%04X"%     moduleEntry.GlblcntUsage )
+            print("   ref count (p)  =     0x%04X"%     moduleEntry.ProccntUsage )
+            print("   base address   = {}".format(moduleEntry.modBaseAddr))
+            print("   base size      = %d"%             moduleEntry.modBaseSize )
 
             process = OpenProcess(0x10, False, mypid)
             returnAddress = c_int32()
             bytesRead = c_int32()
             # int(0x01F9E3D8) is add value from baseAddress of "Engine.dll"
-            ReadProcessMemory(process, addressof(me32.modBaseAddr.contents) + int(0x01F9E3D8), byref(returnAddress), sizeof(returnAddress), byref(bytesRead))
+            ReadProcessMemory(process, addressof(moduleEntry.modBaseAddr.contents) + int(FirstOffSet), byref(returnAddress), sizeof(returnAddress), byref(bytesRead))
             # Return value int of modBaseAddr that using in ReadProcessMemory
             # Just add offset to get correct data value like HP/CP/MP ...
             return returnAddress.value 
-        ret = Module32Next(hModuleSnap, byref(me32))
+        ret = Module32Next(hModuleSnap, byref(moduleEntry))
 
 
     CloseHandle(hModuleSnap)
     return False  
 
-def ReadLineageOffsetValue(ProcessID, ModuleName, *OffSetList):
+def ReadLineageOffsetValueInt32(Process, ModuleName, StartAddress, *OffSetList):
     #Viet ham doc phan cap offset, chu y la cu moi offset trong Cheat Engine tuong ung mot con tro
-    pass
+    retValue = 0
+    offsetAddressNext = StartAddress
+    for offsets in OffSetList:
+        value = c_int32()
+        # Address truyen cho ReadProcessMemory co the la so int, hex...
+        ReadProcessMemory(Process, offsetAddressNext + offsets, byref(value), sizeof(value), None)
+        retValue = value
+        offsetAddressNext = value.value
+    return retValue
+
+
 # main
 if __name__ == '__main__':
     print("")
     print("==================================================")
     print("VI DU CHO RA PROCESS ID TUONG UNG VOI PID 9476 va module 'Engine.dll' chua nhieu thong so game")
     print("--------------------------------------------------")
-    mypid = 304 #PID cua process
+    mypid = 16884 #PID cua process
     process = OpenProcess(0x10, False, mypid)
-    BaseHexAddress = GetBaseAddressModule(mypid,"Engine.dll")
-    while True:
-    # Address truyen cho ReadProcessMemory co the la so int, hex...
-        hp = c_int32()
-        ReadProcessMemory(process, BaseHexAddress + int(0x5C), byref(hp), sizeof(hp), None)
-        mp = c_int32()
-        ReadProcessMemory(process, BaseHexAddress + int(0x60), byref(mp), sizeof(mp), None)
-        cp = c_int32()
-        ReadProcessMemory(process, BaseHexAddress + int(0x64), byref(cp), sizeof(cp), None)
-        print("Info: HP: %s/MP: %s/CP: %s." % (hp.value, mp.value, cp.value))
+    baseEngine = GetBaseAddressModule(mypid, "Engine.dll", 0x01F9E3D8)
+    hp = ReadLineageOffsetValueInt32(process, "Engine.dll", baseEngine, int(0x5C)).value
+    mp = ReadLineageOffsetValueInt32(process, "Engine.dll", baseEngine, int(0x60)).value
+    cp = ReadLineageOffsetValueInt32(process, "Engine.dll", baseEngine, int(0x64)).value
+    hppt = dict()
+    baseNWindow = GetBaseAddressModule(mypid, "NWindow.DLL", 0x0134E620)
+    for i in range(9):
+        hppt[i] = ReadLineageOffsetValueInt32(process, "NWindow.DLL", baseNWindow, int(0x20), int(0x3E8), int(0x214), int(0x444 + i*0x4), int(0x34), int(0x21C)).value
+
     CloseHandle(process)
